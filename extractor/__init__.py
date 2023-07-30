63697fc9d9f54eebc999bf37bfc8bc8a2e01d98c
Sumit Arora <singhsays@gmail.com>
2023-07-30 16:49:41 -0700

    add extract command scaffold.

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,6 +1,24 @@
+from dataclasses import dataclass
+from pathlib import Path
+
 import click
+import dacite
+from loguru import logger as log
 
 
-@click.command()
+@dataclass(kw_only=True)
+class Options:
+    filename: Path | None
+
+
+@click.group()
 def cli(**kwargs):
     pass
+
+
+@cli.command()
+@click.argument("filename")
+def extract(**kwargs):
+    config = dacite.Config(type_hooks={Path: lambda d: Path(d).expanduser().resolve()})
+    options = dacite.from_dict(data_class=Options, data=kwargs, config=config)
+    log.info(f"extracting from {options.filename}")
